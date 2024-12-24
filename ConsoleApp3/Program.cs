5fb6f85e8e0b13503215ba5755eda084d6643614
NANAME002 <dennzerogo@gmail.com>
2024-12-24 19:59:43 +0200

    add3

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {/// <summary>
+     /// Вывести четные двузначные числа и подсчитать их количество.
+     /// </summary>
+     /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+         
+            Console.WriteLine("Четные двузначные числа:");
+            for (int number = 10; number <= 99; number++)
+            {
+                if (IsEven(number))
+                {
+                    Console.WriteLine(number);
+                }
+            }
+
+            
+            int count = CountEvenNumbers(10, 99);
+            Console.WriteLine($"\nКоличество четных двузначных чисел: {count}");
+            Console.ReadKey();
+        }
+
+       
+        static bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+
+        static int CountEvenNumbers(int start, int end)
+        {
+            int count = 0;
+            for (int number = start; number <= end; number++)
+            {
+                if (IsEven(number))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+
+}
