b47ab5eacb04213fb9f8298125e7f2825c582f3f
NANAME002 <dennzerogo@gmail.com>
2024-12-25 10:22:08 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Repeat();
+            Console.ReadLine();
+        }
+        public static void Repeat()
+        {
+            int count = 0;
+            Random random = new Random();
+            for (int i = 0; i < 15; i++)
+            {
+                int number = random.Next(-20, 30);
+                Console.WriteLine(number);
+                if (number > 0) count++;
+                
+            }
+            Console.WriteLine($"количество положительных чисел= {count}");
+
+        }
+    }
+}
