22c2e2d123af68943f59f201053d435c245d15af
NANAME002 <dennzerogo@gmail.com>
2024-12-25 09:54:19 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Repeat();
+            Console.ReadKey();
+
+        }
+        public static void Repeat()
+        {
+            double price = 20.4;
+            for (int i = 2; i<=20;i++)
+            {
+                Console.WriteLine($"{i}шт.={price * i}руб");
+            }
+        }
+    }
+}
