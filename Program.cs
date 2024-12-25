0b66b5f7345ede7aa524ec65c783167cc52649f4
NANAME002 <dennzerogo@gmail.com>
2024-12-25 10:24:21 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Repeat();
+            Console.ReadKey();
+
+        }
+        public static void Repeat()
+        {
+            Random random = new Random();
+            for (int i =0; i <10; i++)
+            {
+                int number = random.Next(-40,40); 
+                Console.WriteLine(number);
+
+            }
+        }
+                
+    }
+}
